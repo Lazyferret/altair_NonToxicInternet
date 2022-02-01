555c6dfb4038fc74e1e71907b05736cd0e77d2f6
Lazyferret <alex162.ru@yandex.ru>
2022-02-01 13:12:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,11 +10,4 @@ var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "POST", 'http://localhost:2000', true ); // false for synchronous request
 xmlHttp.send(data);
 
-//var xmlHttp2 = new XMLHttpRequest();
-//xmlHttp2.open( "POST", 'http://localhost:2000', true ); // false for synchronous request
-//xmlHttp2.send(data);
-//var recieved = JSON.parse(xmlHttp.response);
 console.log(xmlHttp.response);
-//recieved.forEach(function(item, i, arr) {
-//	console.log(recieved[i]);
-//});	
